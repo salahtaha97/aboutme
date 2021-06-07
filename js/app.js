5ae839d8b1de1f3f6e7756d185f8f97f48e3c908
salahtaha97 <sal97taha@gmail.com>
2021-06-07 22:47:21 +0300

    new lab

---
+++ js/app.js
@@ -0,0 +1,45 @@
+let myName = prompt("Is my name salah?");
+console.log(myName);
+if (myName === "yes" && "true") {
+    alert("This corect")
+    console.log("correct")
+}else{
+    alert("it is salah")
+}
+
+let uniStudy = prompt(" did I study at hashimate university?");
+if (uniStudy === "yes" && "true") {
+    alert("This corect")
+    console.log("correct")
+}else{
+    alert("I was stuidied at it.")
+}
+
+let jobExp = prompt("Did i work as a customer service previsoly?")
+if (jobExp === "yes" && "true") {
+    alert("This corect")
+    console.log("correct")
+}else{
+    alert("I worked as customer service for 6 months")
+}
+
+let grYear = prompt("Is 2019 my graduation year??");
+if (grYear === "yes" && "true") {
+    alert("This corect")
+    console.log("correct")
+}else{
+    alert("I graduted at feb/2019")
+}
+
+let bnYear = prompt("Am I 24 years old??");
+if (bnYear === "yes" && "true") {
+    alert("This corect")
+    console.log("correct")
+}else{
+    alert("I was born in 1997")
+}
+
+let userName = prompt("What is your name?");
+console.log(userName);
+alert("welcome " + userName);
+alert("good bye" + userName)

2ad99195b0bcc2094e8ed406a24855276115d4d4
salah taha <84705790+salahtaha97@users.noreply.github.com>
2021-06-08 22:28:53 +0300

    Update app.js
    add new element and loop

--- js/app.js
+++ js/app.js
@@ -1,45 +1,41 @@
+let answEr = 0
+let userName = prompt("What is your name?");
+console.log(userName);
+alert("welcome " + userName);
+
 let myName = prompt("Is my name salah?");
 console.log(myName);
 if (myName === "yes" && "true") {
     alert("This corect")
     console.log("correct")
-}else{
+    answEr++
+} else {
     alert("it is salah")
 }
 
 let uniStudy = prompt(" did I study at hashimate university?");
-if (uniStudy === "yes" && "true") {
-    alert("This corect")
+if (uniStudy === "yes" || uniStudy === "true") {
+    alert("This correct")
     console.log("correct")
-}else{
+    answEr++
+} else {
     alert("I was stuidied at it.")
 }
 
 let jobExp = prompt("Did i work as a customer service previsoly?")
-if (jobExp === "yes" && "true") {
-    alert("This corect")
+if (jobExp === "yes" || jobExp === "true") {
+    alert("This correct")
     console.log("correct")
-}else{
+    answEr++
+} else {
     alert("I worked as customer service for 6 months")
 }
 
 let grYear = prompt("Is 2019 my graduation year??");
-if (grYear === "yes" && "true") {
-    alert("This corect")
+if (grYear === "yes" || grYear === "true") {
+    alert("This correct")
     console.log("correct")
-}else{
+    answEr++
+} else {
     alert("I graduted at feb/2019")
 }
-
-let bnYear = prompt("Am I 24 years old??");
-if (bnYear === "yes" && "true") {
-    alert("This corect")
-    console.log("correct")
-}else{
-    alert("I was born in 1997")
-}
-
-let userName = prompt("What is your name?");
-console.log(userName);
-alert("welcome " + userName);
-alert("good bye" + userName)
